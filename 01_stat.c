275e657c88b9a6f9bf2665798737b6d213cc75d5
Andrew <82667860+Pandnak@users.noreply.github.com>
2021-12-13 09:56:52 +0300

    Update 01_stat.c

--- 01_stat.c
+++ 01_stat.c
@@ -1,51 +1,12 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include<fcntl.h>
-#include<time.h>
-
-const char* typeFile(const mode_t mode)
-{
-    switch(mode & S_IFMT)
-    {
-        case S_IFBLK:  return "block device";         break;
-        case S_IFCHR:  return "character device";     break;
-        case S_IFDIR:  return "directory";            break;
-        case S_IFIFO:  return "FIFO/pipe";            break;
-        case S_IFLNK:  return "symlink";              break;
-        case S_IFREG:  return "regular file";         break;
-        case S_IFSOCK: return "socket";               break;
-        default:       return "unknown?";             break;
-    }
-}
-
-void print_access(const struct stat *sb)
-{
-    printf("                  Access:(%u/", sb -> st_mode);
-	putchar( (S_ISDIR(sb->st_mode)) ? 'd' : '-');
-	putchar( (sb->st_mode & S_IRUSR) ? 'r' : '-');
-    putchar( (sb->st_mode & S_IWUSR) ? 'w' : '-');
-   	putchar( (sb->st_mode & S_IXUSR) ? 'x' : '-');
-   	putchar( (sb->st_mode & S_IRGRP) ? 'r' : '-');
-   	putchar( (sb->st_mode & S_IWGRP) ? 'w' : '-');
-  	putchar( (sb->st_mode & S_IXGRP) ? 'x' : '-');
-  	putchar( (sb->st_mode & S_IROTH) ? 'r' : '-');
-	putchar( (sb->st_mode & S_IWOTH) ? 'w' : '-');
-    putchar( (sb->st_mode & S_IXOTH) ? 'x' : '-');
-    printf(")\n");
-}
-
-
-void printf_time(const time_t *tmp)
-{
-     struct tm bdt;
-     char s[50];
-     ctime_r(tmp, s);
-     localtime_r(tmp, &bdt);
-     printf("%s %s", bdt.tm_zone, s);        
-}
+#include <fcntl.h>
+#include <time.h>
+//-----------------------------------------------------
+#include "../funcs.h"
 
 int main(int argc, char* argv[])
 {
@@ -64,9 +25,7 @@ int main(int argc, char* argv[])
         perror("lstat");
         exit(EXIT_FAILURE);
 
-    }
-    
-    fd = open(argv[1], O_RDONLY);
+    }  
 
     printf("            Name of file: %s   %s\n", argv[1], typeFile(sb.st_mode));
         
@@ -88,6 +47,5 @@ int main(int argc, char* argv[])
     printf("             Last change: ");
     printf_time(&sb.st_ctime);
 
-    printf("         File descriptor: %d\n", fd);
     exit(EXIT_SUCCESS);
 }

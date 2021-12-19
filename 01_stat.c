73ed02ab0769238ede13b8254d904312f04de8a7
andrey <mr.pandak@mail.ru>
2021-12-19 18:59:30 +0300

    Программы приведены к одному формату

--- 01_stat.c
+++ 01_stat.c
@@ -1,50 +1,47 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include <sys/types.h>
 #include <time.h>
+#include <unistd.h>
 //-----------------------------------------------------
-#include "../funcs.h"
+#include "./funcs.h"
 
-int main(int argc, char* argv[])
-{
-    struct stat sb;
+int main(int argc, char *argv[]) {
+	struct stat sb;
 
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <pathname>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s <pathname>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+	if (lstat(argv[1], &sb) == -1) {
+		perror("lstat");
+		exit(EXIT_FAILURE);
+	}
 
-    if (lstat(argv[1], &sb) == -1)
-    {
-        perror("lstat");
-        exit(EXIT_FAILURE);
+	printf("            Name of file: %s   %s\n", argv[1],
+		   typeFile(sb.st_mode));
 
-    }  
+	printf("            Size of file: %ld\n", sb.st_size);
+	printf("Recomended size of block: %ld\n", sb.st_blksize);
+	printf("        Amount of blocks: %ld\n", sb.st_blocks);
 
-    printf("            Name of file: %s   %s\n", argv[1], typeFile(sb.st_mode));
-        
-    printf("            Size of file: %ld\n", sb.st_size);
-    printf("Recomended size of block: %ld\n", sb.st_blksize);
-    printf("        Amount of blocks: %ld\n", sb.st_blocks);
+	printf("                  Device: %lu, I-node: %lu, Links: %lu \n",
+		   sb.st_dev, sb.st_ino, sb.st_nlink);
 
-    printf("                  Device: %lu, I-node: %lu, Links: %lu \n", sb.st_dev, sb.st_ino, sb.st_nlink);
-    
-    printf("                     Uid: %u\n", sb.st_uid);
-    printf("                     Gid: %u\n", sb.st_gid);
-    
-    print_access(&sb);
-    
-    printf("             Last access: ");
-    printf_time(&sb.st_atime);
-    printf("        Last modifaction: ");
-    printf_time(&sb.st_mtime);
-    printf("             Last change: ");
-    printf_time(&sb.st_ctime);
+	printf("                     Uid: %u\n", sb.st_uid);
+	printf("                     Gid: %u\n", sb.st_gid);
 
-    exit(EXIT_SUCCESS);
+	print_access(&sb);
+
+	printf("             Last access: ");
+	printf_time(&sb.st_atime);
+	printf("        Last modifaction: ");
+	printf_time(&sb.st_mtime);
+	printf("             Last change: ");
+	printf_time(&sb.st_ctime);
+
+	exit(EXIT_SUCCESS);
 }
--- 14_4_1.c
+++ 14_4_1.c
@@ -1,62 +1,61 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h> //for clock_gettime
-#include <stdio.h>   // for perrror
-#include <unistd.h>  // for write 
-#include <string.h>
-#include <stdint.h> // for uint8_t
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <stdint.h> // for uint8_t
+#include <stdio.h>
+#include <stdio.h> // for perrror
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>	//for clock_gettime
+#include <unistd.h> // for write
 
 #include "./funcs.h"
 
 #define MAX_LEN 1000
 const size_t count = 5; // number of  bytes are read for one cycle
 
-int main(void)
-{
-    unsigned long nbytes = 0;
-    void * buf = malloc(MAX_LEN);
-    
-    FILE * ptrFile = fopen("stderr.txt" , "wb");
-    if (ptrFile == NULL)
-    {
-        perror("Wrong of file");
-        return RESULT_OPEN_FAILED; 
-    }
-            
-    struct timespec tmStart;  //Тут компилятор требовал инициализацию времен, 
-    struct timespec tmEnd;   //хотя они все равно в них потом все равно записывается время
-    //use clock_gettime
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &tmStart);
-    
-    printf("%ld\n", (- (tmEnd.tv_nsec)  + tmStart.tv_nsec)%1000000000);
-    while(1)
-    {
-        if (nbytes += (read(fileno(stdin), buf, count)) == 0)
-        {
-            if (ferror(stdin) != 0)
-            {
-                fprintf(stderr, "%lu bytes read\n", nbytes);
-                perror("Wrong reading stdin");
-                free(buf);
-                return RESULT_BAD_READ;
- 
-            }
-            if (feof(stdout) != 0)
-            {
-                fprintf(stderr, "%lu bytes read\n", nbytes);
-                break;
-            }
-        }
-    }
-    
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &tmEnd);
-    
-    fprintf(stderr, "%ld.%06ld\n", (tmEnd.tv_nsec  - tmStart.tv_nsec)/1000000000, 
-                       (tmEnd.tv_nsec  - tmStart.tv_nsec)%1000000000/1000); //nsec in long
-    free(buf);
-    fclose(ptrFile);
-    return RESULT_OK;
+int main(void) {
+	unsigned long nbytes = 0;
+	void *buf = malloc(MAX_LEN);
+
+	FILE *ptrFile = fopen("stderr.txt", "wb");
+	if (ptrFile == NULL) {
+		perror("Wrong of file");
+		return RESULT_OPEN_FAILED;
+	}
+
+	struct timespec tmStart; //Тут компилятор требовал инициализацию времен,
+	struct timespec
+		tmEnd; //хотя они все равно в них потом все равно записывается время
+	// use clock_gettime
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &tmStart);
+
+	// printf("%ld\n", (- (tmEnd.tv_nsec)  + tmStart.tv_nsec)%1000000000);
+	while (1) {
+		if (nbytes += (read(fileno(stdin), buf, count)) == 0) {
+			if (ferror(stdin) != 0) {
+				// printf("%lu\n", nbytes);
+				fprintf(ptrFile, "%lu bytes read\n", nbytes);
+				perror("Wrong reading stdin");
+				free(buf);
+				return RESULT_BAD_READ;
+			}
+			if (feof(stdin) != 0) {
+				// printf("%lu\n", nbytes);
+				fprintf(ptrFile, "%lu bytes read\n", nbytes);
+				break;
+			}
+		}
+		// printf("%lu\n", nbytes);
+	}
+	printf("%lu\n", nbytes);
+
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &tmEnd);
+
+	fprintf(
+		ptrFile, "%ld.%06ld\n", (tmEnd.tv_nsec - tmStart.tv_nsec) / 1000000000,
+		(tmEnd.tv_nsec - tmStart.tv_nsec) % 1000000000 / 1000); // nsec in long
+	free(buf);
+	fclose(ptrFile);
+	return RESULT_OK;
 }

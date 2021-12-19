edf50b4f7c881dd9cc4cbc8721d83976273e00a3
andrey <mr.pandak@mail.ru>
2021-12-19 21:17:35 +0300

    Дополненный вариант 3й задачи с добавленными функциями

--- funcs.c
+++ funcs.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <sys/types.h>
@@ -7,8 +8,9 @@
 #include <fcntl.h>                
 #include <sys/stat.h>
 #include <time.h>
+#include <assert.h>
 // --------------------------------------------------------------------------------------------
-#include "funcs.h"
+#include "./funcs.h"
 
 
 //function defines type of file
@@ -43,6 +45,16 @@ void print_access(const struct stat *sb)
     printf(")\n");
 }
 
+// the function of the removing fire from the current dir
+int rm_file(const char* filename) {
+
+    if (unlink(filename) == -1) {
+        perror("It's not possible to remove this file");
+        return RESULT_ERR;
+    }
+    printf("[+] Successful removing: %s", filename);
+    return RESULT_OK;
+}
 
 void printf_time(const time_t *tmp)
 {
@@ -74,4 +86,49 @@ ssize_t writeall(int fd, const void *buf, size_t count)
     }
     return (ssize_t)bytes_written;
 }
- 
+
+int my_copy_files(unsigned source_fd, unsigned dest_fd, const char* dest_name, long long size_source, const unsigned max_len)
+{
+     // Make buffer for copy after
+    char* buf = (char*) calloc(max_len, sizeof(char));
+    assert(buf != NULL);
+
+    //Copy
+    while(size_source  > 0) 
+    {
+
+        ssize_t read_symb_amount = read(source_fd, buf, max_len);
+
+        if (read_symb_amount < 0) 
+        {
+            perror("Failed read from the file");
+            rm_file(dest_name);
+            close(source_fd);
+            return RESULT_BAD_READ;
+        }
+
+        ssize_t write_symb_amount = writeall(dest_fd, buf, read_symb_amount);
+
+        if (write_symb_amount < 0) 
+        {
+            perror("Failed write to file");
+            rm_file(dest_name);
+            close(dest_fd);
+            return RESULT_BAD_WRITE;
+        }
+
+        if (write_symb_amount != read_symb_amount) 
+        {
+            perror("The number of symbols written does't match the number of symbols read.");
+            rm_file(dest_name);
+            close(dest_fd);
+            return RESULT_BAD_WRITE;
+        }
+
+        size_source -= read_symb_amount;
+    }
+
+    free(buf);
+
+    return RESULT_OK;
+} 
--- funcs.h
+++ funcs.h
@@ -6,6 +6,7 @@ enum {
     RESULT_BAD_READ,
     RESULT_BAD_WRITE,
     RESULT_BAD_CLOSE,
+    RESULT_BAD_STAT,
     RESULT_BAD_FILE_DELETE,
     RESULT_BAD_COPY_TIME,
     RESULT_BAD_READLINK,
@@ -16,8 +17,12 @@ enum {
 
 const char* typeFile(const mode_t mode);
 
+int rm_file(const char* filename);
+
 void print_access(const struct stat *sb);
 
 ssize_t writeall(int fd, const void *buf, size_t count);
 
 void printf_time(const time_t *tmp);
+
+int my_copy_files(unsigned source_fd, unsigned dest_fd, const char* dest_name, long long size_source, const unsigned max_len);
